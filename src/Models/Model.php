4bd55bf9beabb798d77766fdb8605e74c8bbd38e
Mikolaj <mikolaj@evoware.io>
2024-04-11 01:47:59 +0200

    Cleaned up Model. Replaced public properties with getters/setters.

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -7,37 +7,110 @@ use DateTime;
 class Model
 {
     /** @var string Model name */
-    public string $name;
+    protected string $name;
 
     /** @var string the model ID */
-    public string $id;
+    protected string $id;
 
     /** @var int Size in megabytes */
-    public int $size;
+    protected int $size;
 
     /** @var string Model version */
-    public string $version;
+    protected string $version;
 
     /** @var string Model digest */
-    public string $digest;
+    protected string $digest;
 
     /** @var string Model version */
-    public array $details;
+    protected array $details;
 
-    public DateTime $modifiedAt;
+    /** @var DateTime */
+    protected DateTime $modifiedAt;
 
     public function __construct(array $data)
     {
-        $this->name = $data['name'];
-        $this->id = $data['model'];
-        $this->size = $data['size'];
-        $this->digest = $data['digest'];
-        $this->modifiedAt = DateTime::createFromFormat(DateTime::ATOM, $data['modified_at']);
-        $this->details = $data['details'];
+        // validate all fields in minimalist way
+        foreach (['name', 'model', 'size', 'digest', 'modified_at', 'details'] as $key) {
+            if (!isset($data[$key]) || empty($data[$key])) {
+                throw new \Exception('Missing required field: ' . $key);
+            }
+        }
+
+        $this->setName($data['name']);
+        $this->setId($data['model']);
+        $this->setSize($data['size']);
+        $this->setDigest($data['digest']);
+        $this->modifiedAt = DateTime::createFromFormat('Y-m-d\TH:i:sP', $data['modified_at'] ?? DateTime::createFromFormat(DateTime::ATOM, $data['modified_at']));
+        $this->setDetails($data['details']);
     }
 
     public function __toString(): string
     {
+        return $this->getName();
+    }
+
+    public function getName(): string
+    {
         return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getSize(): int
+    {
+        return $this->size;
+    }
+
+    public function setSize(int $size): void
+    {
+        $this->size = $size;
+    }
+
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+    public function setVersion(string $version): void
+    {
+        $this->version = $version;
+    }
+
+    public function getDigest(): string
+    {
+        return $this->digest;
+    }
+
+    public function setDigest(string $digest): void
+    {
+        $this->digest = $digest;
+    }
+
+    public function getDetails(): array
+    {
+        return $this->details;
+    }
+
+    public function setDetails(array $details): void
+    {
+        $this->details = $details;
+    }
+
+    public function getModifiedAt(): DateTime
+    {
+        return $this->modifiedAt;
     }
 }
